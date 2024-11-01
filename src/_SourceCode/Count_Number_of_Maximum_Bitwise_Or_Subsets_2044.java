a6155328c761e2504bd56be75737a2636976576e
diamondbusiness <tungnd21896@gmail.com>
2024-11-01 09:03:18 +0700

    1/11/2024

---
+++ src/_SourceCode/Count_Number_of_Maximum_Bitwise_Or_Subsets_2044.java
@@ -0,0 +1,28 @@
+package _SourceCode;
+
+public class Count_Number_of_Maximum_Bitwise_Or_Subsets_2044 {
+    public void backtrack(int[] nums, int index, int currentOR, int maxOR, int[] count) {
+        if (currentOR == maxOR) {
+            count[0]++;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            backtrack(nums, i + 1, currentOR | nums[i], maxOR, count);
+        }
+    }
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOR = 0;
+
+        // Step 1: Compute the maximum OR
+        for (int num : nums) {
+            maxOR |= num;
+        }
+
+        int[] count = new int[1];
+        // Step 2: Backtrack to count the subsets
+        backtrack(nums, 0, 0, maxOR, count);
+
+        return count[0];
+    }
+}
