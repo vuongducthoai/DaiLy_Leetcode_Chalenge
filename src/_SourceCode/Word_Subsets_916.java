d1f3a2db116937108d226b6e48699664570999d5
vuongducthoai <thoai12309@gmail.com>
2025-01-10 20:13:25 +0700

    10/1/2024

---
+++ src/_SourceCode/Word_Subsets_916.java
@@ -0,0 +1,48 @@
+package _SourceCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Word_Subsets_916 {
+    public static List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] maxCharFreq = new int[26];
+        int[] tempCharFreq = new int[26];
+
+        for (String word : words2){
+            Arrays.fill(tempCharFreq, 0);
+            for (char ch : word.toCharArray()){
+                tempCharFreq[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; i ++){
+                maxCharFreq[i] = Math.max(maxCharFreq[i], tempCharFreq[i]);
+            }
+        }
+
+        List<String> universalWords  = new ArrayList<>();
+        for (String word : words1){
+            Arrays.fill(tempCharFreq, 0);
+            for (char ch : word.toCharArray()){
+                tempCharFreq[ch - 'a']++;
+            }
+
+            boolean isUniversal = true;
+            for (int i = 0; i < 26; i ++){
+                if (maxCharFreq[i] > tempCharFreq[i]){
+                    isUniversal = false;
+                    break;
+                }
+            }
+            if (isUniversal){
+                universalWords.add(word);
+            }
+        }
+        return universalWords;
+    }
+
+    public static void main(String[] args) {
+        String[] words1 = {"amazon", "facebook", "google", "leetcode"};
+        String[] words2 = {"e", "o"};
+        System.out.println(wordSubsets(words1, words2));
+    }
+}
