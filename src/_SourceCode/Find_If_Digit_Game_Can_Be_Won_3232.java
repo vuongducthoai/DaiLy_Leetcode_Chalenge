c7367b821231cb9e78a6a5b02d5146db6672b6d1
vuongducthoai <thoai12309@gmail.com>
2025-09-26 23:19:25 +0700

    26/09/2025

--- src/_SourceCode/Find_If_Digit_Game_Can_Be_Won_3232.java
+++ src/_SourceCode/Find_If_Digit_Game_Can_Be_Won_3232.java
@@ -2,11 +2,20 @@ package _SourceCode;
 
 public class Find_If_Digit_Game_Can_Be_Won_3232 {
     public static boolean canAliceWin(int[] nums) {
-        return true;
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int num : nums){
+            if(num < 10){
+                sum1 += num;
+            } else {
+                sum2 += num;
+            }
+        }
+        return sum1 != sum2;
     }
 
     public static void main(String[] args) {
-        int nums[] = {1, 2, 3, 4, 10};
+        int nums[] = {1,2, 3, 4, 5, 14};
         System.out.println(canAliceWin(nums));
     }
 }
